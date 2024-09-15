c43872a2880c6121fb73e6d2889ad7a8983780e2
souta simono <souta.ss.simono@icloud.com>
2024-09-15 13:40:40 +0900

    プロフィールコンポーネントの追加

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,22 @@
-function Layout() {
+import Head from "next/head";
+import styles from "./Layout.module.css";
+import utilsStyles from "../styles/utils.module.css";
+
+
+const name = "Shin Code";
+export const siteTitle = "Next.js Blog";
+
+function Layout( {children} ) {
     return (
-        <div>
-            Enter
+        <div className={styles.container}>
+            <Head>
+                <link ref="icon" href="favicon.ico"/>
+            </Head>
+            <header className={styles.header}>
+                <img src="/images/profile.png" className={utilsStyles.borderCircle}/>
+                <h1 className={utilsStyles.heading2Xl}>{name}</h1>
+            </header>
+            <main>{children}</main>
         </div>
     );
 }
