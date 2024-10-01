15cc897be0cc1ac47ae964a568b0bb3cdc1636e8
souta simono <souta.ss.simono@icloud.com>
2024-10-01 16:37:53 +0900

    ブログidに応じた記事内容を返す関数の作成

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,18 @@
 import Layout from "@/components/Layout";
+import { getAllPostsIds } from "../../lib/post";
+
+export async function getStaticPaths() {
+    const paths = getAllPostsIds();
+    
+    return {
+        paths,
+        fallback:false,//falseは存在しないidの場合404を返す
+    }
+}
+
+export function getStaticProps({params}){
+    
+}
 
 export default function Post() {
     return (
