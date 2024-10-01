4c46abb48baac5fcdccc8462fa37349ad59e5953
souta simono <souta.ss.simono@icloud.com>
2024-10-01 15:05:10 +0900

    getStaticPropsを使ってSSGの実装

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,19 @@ import styles from "@/styles/Home.module.css";
 import Link from "next/link";
 import Layout from "@/components/Layout";
 import UtilStyle from "../styles/utils.module.css";
+import {getPostsData} from '../lib/post';
+
+//SSG(静的生成)の場合
+//getStaticPropsはnext標準関数(外部から一度だけデータを取ってくるとき使う関数)
+export async function getStaticProps(){
+  const allPostsData = getPostsData(); 
+  console.log(allPostsData);
+  return{
+    props:{
+      allPostsData,
+    },
+  };
+}
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -17,7 +30,7 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export default function Home() {
+export default function Home({allPostsData}) {
   return (
     <Layout>  
       <section className={UtilStyle.headingMd}>
